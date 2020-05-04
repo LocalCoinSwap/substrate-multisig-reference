6122f1781e913f5ff993cebe42df35a035b62f88
codeforcer <nathancworsley@gmail.com>
2020-05-04 17:03:00 +0100

    remove unused file

--- mnemonic.py
+++
@@ -1,6 +0,0 @@
-test_mnemonic = """
-prefer fashion insect dizzy energy marble
-forget artefact aspect short surface leave
-"""
-
-expected_initial_address = "Ff4gBd7WcHgsNVhr5HGPQXQx4PzPHGtHdNVaCRK5d5KeMHh"
