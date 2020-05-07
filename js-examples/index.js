736040bcfd696bc8e75df3323d9dd9f1693ea1ca
codeforcer <nathancworsley@gmail.com>
2020-05-07 20:39:44 +0100

    example actually works

--- js-examples/index.js
+++ js-examples/index.js
@@ -3,24 +3,26 @@
 /* eslint-disable @typescript-eslint/unbound-method */
 
 // Import the API, Keyring and some utility functions
+import { cryptoWaitReady } from '@polkadot/util-crypto';
 const { ApiPromise, WsProvider } = require('@polkadot/api');
 const { Keyring } = require('@polkadot/keyring');
 
 // Instantiate the API
 const wsProvider = new WsProvider('wss://kusama-rpc.polkadot.io/');
-const api = await ApiPromise.create({ provider: wsProvider });
 
-// Constuct the keying after the API (crypto has an async init)
-const keyring = new Keyring({ type: 'sr25519' });
-
-const sellerKey = keyring.addFromSeed(
-  Buffer.from('8b3585743475df08238ff09c401ff8639171c9ad2584045f4b23a5e4f5e326f8', 'hex'));
-const buyerKey = keyring.addFromSeed(
-    Buffer.from('64c29cbfc0cec793d1c62a5d80261aa8a1c03535379cedbb0601c1f89ad2271e', 'hex'));
-const adminKey = keyring.addFromSeed(
-    Buffer.from('2cf02efa64eb4ba75fa2f42f69dcf5548e821f1e852e2b26aa1b09d5eda0065a', 'hex'));
+// const buyerKey = keyring.addFromSeed(
+//     Buffer.from('64c29cbfc0cec793d1c62a5d80261aa8a1c03535379cedbb0601c1f89ad2271e', 'hex'));
+// const adminKey = keyring.addFromSeed(
+//     Buffer.from('2cf02efa64eb4ba75fa2f42f69dcf5548e821f1e852e2b26aa1b09d5eda0065a', 'hex'));
 
 async function fundEscrow () {
+  const api = await ApiPromise.create({ provider: wsProvider });
+
+  await cryptoWaitReady();
+    const keyring = new Keyring({ type: 'sr25519' });
+  const sellerKey = keyring.addFromSeed(
+    Buffer.from('8b3585743475df08238ff09c401ff8639171c9ad2584045f4b23a5e4f5e326f8', 'hex'));
+
   const escrowAddress = 'HFXXfXavDuKhLLBhFQTat2aaRQ5CMMw9mwswHzWi76m6iLt';
   const transfer = api.tx.balances.transfer(escrowAddress, 100000000000);
 
@@ -30,8 +32,13 @@ async function fundEscrow () {
   console.log('Transfer sent with hash', hash.toHex());
 }
 
-// IN PROGRESS
 async function asMulti () {
+    const api = await ApiPromise.create({ provider: wsProvider });
+
+    await cryptoWaitReady();
+    const keyring = new Keyring({ type: 'sr25519' });
+    
+
     const escrowAddress = 'HFXXfXavDuKhLLBhFQTat2aaRQ5CMMw9mwswHzWi76m6iLt';
     const tx = api.tx.utility.asMulti(
         );
