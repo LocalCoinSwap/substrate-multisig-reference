b3a1567dc430adac557f18bc3056b81418fd83a8
codeforcer <nathancworsley@gmail.com>
2020-05-08 23:57:37 +0100

    successful demonstration of approve_as_multi

--- js-examples/config.js
+++ js-examples/config.js
@@ -1,4 +1,12 @@
 require('dotenv').config()
+const { Keyring } = require('@polkadot/keyring');
+import { cryptoWaitReady } from '@polkadot/util-crypto';
+import {
+    encodeAddress,
+    keyFromPath,
+    schnorrkelKeypairFromSeed,
+    mnemonicToMiniSecret
+} from '@polkadot/util-crypto';
 
 export const kusamaNode = 'wss://kusama-rpc.polkadot.io/';
 
@@ -6,8 +14,23 @@ export const kusamaNode = 'wss://kusama-rpc.polkadot.io/';
 export const sellerHexSeed = process.env.SELLER_HEX_SEED;
 export const adminHexSeed = process.env.ADMIN_HEX_SEED;
 export const buyerHexSeed = process.env.BUYER_HEX_SEED;
+
+export async function deriveAddress(hexSeed) {
+  await cryptoWaitReady();
+  const { publicKey } = keyFromPath(
+    schnorrkelKeypairFromSeed(
+      Buffer.from(hexSeed, 'hex')),
+    [],
+    'sr25519');
+  return encodeAddress(publicKey, 2)
+}
+
+async function deriveEscrowAddress(addresses) {
+
+}
+
 export const buyerAddress = 'CdVuGwX71W4oRbXHsLuLQxNPns23rnSSiZwZPN4etWf6XYo';
 export const adminAddress = 'HvqnQxDQbi3LL2URh7WQfcmi8b2ZWfBhu7TEDmyyn5VK8e2';
 export const sellerAddress = 'J9aQobenjZjwWtU2MsnYdGomvcYbgauCnBeb8xGrcqznvJc';
 export const escrowAddress = 'HFXXfXavDuKhLLBhFQTat2aaRQ5CMMw9mwswHzWi76m6iLt';
-export const tradeValue = 25000000000;
+export const tradeValue = 10000000000;
