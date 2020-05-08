003aaef5282e6d562929d34d20046be194a394d4
Ravi Ojha <ivarojha@gmail.com>
2020-05-08 22:25:12 +0530

    Use dotenv for secret values

--- js-examples/config.js
+++ js-examples/config.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 export const kusamaNode = 'wss://kusama-rpc.polkadot.io/';
 
 // Trade variables
-export const sellerHexSeed = '8b3585743475df08238ff09c401ff8639171c9ad2584045f4b23a5e4f5e326f8';
-export const adminHexSeed = '2cf02efa64eb4ba75fa2f42f69dcf5548e821f1e852e2b26aa1b09d5eda0065a';
-export const buyerHexSeed = '64c29cbfc0cec793d1c62a5d80261aa8a1c03535379cedbb0601c1f89ad2271e';
+export const sellerHexSeed = process.env.SELLER_HEX_SEED;
+export const adminHexSeed = process.env.ADMIN_HEX_SEED;
+export const buyerHexSeed = process.env.BUYER_HEX_SEED;
 export const buyerAddress = 'CdVuGwX71W4oRbXHsLuLQxNPns23rnSSiZwZPN4etWf6XYo';
 export const adminAddress = 'HvqnQxDQbi3LL2URh7WQfcmi8b2ZWfBhu7TEDmyyn5VK8e2';
 export const sellerAddress = 'J9aQobenjZjwWtU2MsnYdGomvcYbgauCnBeb8xGrcqznvJc';
