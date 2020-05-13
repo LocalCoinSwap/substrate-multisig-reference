8d7db36327d481eb124fd32fbabc4a8ea7e6c4be
Nathan <31618931+CodeForcer@users.noreply.github.com>
2020-05-13 01:52:52 +0100

    flesh out transfer function (#12)
    * flesh out transfer function
    
    * break up approveAsMulti test in composite partsusing tx-wrapper
    
    * successfully make basic transfer using Python

--- js-examples/approve_as_multi.js
+++ js-examples/approve_as_multi.js
@@ -7,13 +7,21 @@ const { cryptoWaitReady } = require('@polkadot/util-crypto');
 const { ApiPromise, WsProvider } = require('@polkadot/api');
 const { Keyring } = require('@polkadot/keyring');
 
+
+import {
+  createSigningPayload,
+  getRegistry,
+  methods,
+  createSignedTx,
+} from '@substrate/txwrapper';
+
 import {
   kusamaNode,
   sellerHexSeed,
   adminHexSeed,
   buyerHexSeed,
   deriveAddress,
-  deriveEscrowAddress,
+  // deriveEscrowAddress,
   tradeValue,
 } from './config';
 
@@ -35,26 +43,70 @@ async function sellerFundEscrow () {
   const sellerKey = keyring.addFromSeed(
     Buffer.from(sellerHexSeed, 'hex'));
 
-  // Make an escrow funding transaction, waiting for inclusion
-  const tx = api.tx.balances.transfer(escrowAddress, tradeValue)
+  const sellerAddress = await deriveAddress(sellerHexSeed)
+
+  // Get generic blockchain and account data
+  const { number, hash} = await api.rpc.chain.getHeader();
+  const genesisHash = await api.rpc.chain.getBlockHash([0]);
+  const { specVersion } = await api.rpc.state.getRuntimeVersion();
+  const { nonce } = await api.query.system.account(sellerAddress);
+  
+  // Parse generic data into JSON value format
+  const latestBlockNumber = parseInt(number._raw, 10);
+  const latestBlockHash = hash.toString('hex');
+  const genesisBlockHash = genesisHash.toString('hex');
+  const spec = parseInt(specVersion, 10);
+
+  // Returns huge away of data about the blockchain
+  const metadataRpc = await api.rpc.state.getMetadata();
+
+  // Appears to give an object with functions relevant to the chain
+  const registry = getRegistry('Kusama', 'kusama', spec);
+
+  // Unsigned JSON
+  const unsigned = methods.balances.transfer(
+    {
+      value: tradeValue,
+      dest: escrowAddress,
+    },
+    {
+      address: sellerAddress,
+      blockHash: latestBlockHash,
+      blockNumber: latestBlockNumber,
+      eraPeriod: 64,  // Kusama specific variable
+      genesisHash: genesisBlockHash,
+      metadataRpc,
+      nonce,
+      specVersion: spec,
+      tip: 0,
+    },
+    {
+      metadata: metadataRpc,
+      registry,
+    }
+  );
+
+  // Construct the signing payload from an unsigned transaction.
+  const signingPayload = registry.createType('ExtrinsicPayload', unsigned, { version: unsigned.version }).toHex();
+  console.log(`\nB: Payload to Sign: ${signingPayload}`);
+
+  // Sign a payload. This operation should be performed on an offline device.
+  const { signature } = registry.createType('ExtrinsicPayload', signingPayload, { version: 4 }).sign(sellerKey);
+  console.log(`\nSignature: ${signature}`);
+
+  // Serialize a signed transaction.
+  const tx = createSignedTx(unsigned, signature, { registry });
+  console.log(`\nTransaction to Submit: ${tx}`);
 
   const promise = new Promise((resolve, reject) => {
-    tx.signAndSend(sellerKey, ({ events = [], status }) => {
-      console.log(`Current transaction status is ${status.type}`)
-      if (status.isFinalized) {
-        console.log(`Transaction was included at blockHash ${status.asFinalized}`);
-        // Loop through Vec<EventRecord> to display all events
-        events.forEach(({ phase, event: { data, method, section } }) => {
-          console.log(`\t' ${phase}: ${section}.${method}:: ${data}`);
-        });
-
-        resolve(status.asFinalized);
-      }
+    api.rpc.author.submitExtrinsic(tx, (x) => {
+      console.log(`Blockchain tx hash for submitted transaction: ${x}`)
+      resolve(x);
     });
   });
 
-  const hash = await promise;
-  return hash;
+  const submittedHash = await promise;
+  return submittedHash;
 }
 
 
@@ -131,9 +183,8 @@ async function adminFinalizeRelease (signerHexSeed, otherSignatories, destAddres
   const hash = await promise;
   return hash;
 }
+
 // TODO: Automate ordering of signatories to prevent production errors
-// TODO: What to do on failure
-// TODO: Handling batching
 async function standardTrade () {
   const buyerAddress = await deriveAddress(buyerHexSeed);
   const sellerAddress = await deriveAddress(sellerHexSeed);
