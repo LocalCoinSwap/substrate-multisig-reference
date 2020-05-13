23ba27cbb7940084ee5ef4eedd8aed3450bb5ed5
codeforcer <nathancworsley@gmail.com>
2020-05-13 13:35:08 +0100

    as_multi broadcasts successfully with custom timepoints

--- tests/test_trade_transactions.py
+++ tests/test_trade_transactions.py
@@ -23,9 +23,9 @@ admin_keypair = sr25519.pair_from_seed(bytes.fromhex(settings.admin_hexseed))
 class TestSimpleSend(unittest.TestCase):
     def test_simple_send(self):
         # Key derivations for test
-        sending_priv = seller_keypair[1].hex()
-        sending_address = ss58_encode(seller_keypair[0], 2)
-        sending_pub = ss58_decode(sending_address)
+        seller_priv = seller_keypair[1].hex()
+        seller_address = ss58_encode(seller_keypair[0], 2)
+        seller_pub = ss58_decode(seller_address)
 
         substrate.init_runtime(block_hash=None)
 
@@ -41,7 +41,7 @@ class TestSimpleSend(unittest.TestCase):
             }
         )
 
-        response = substrate.get_runtime_state("System", "Account", [sending_address])
+        response = substrate.get_runtime_state("System", "Account", [seller_address])
         assert response.get("result")
         nonce = response["result"].get("nonce", 0)
         genesis_hash = substrate.get_block_hash(0)
@@ -71,7 +71,7 @@ class TestSimpleSend(unittest.TestCase):
             data = data.encode()
 
         signature = sr25519.sign(
-            (bytes.fromhex(sending_pub), bytes.fromhex(sending_priv)), data
+            (bytes.fromhex(seller_pub), bytes.fromhex(seller_priv)), data
         )
 
         signature = "0x{}".format(signature.hex())
@@ -83,7 +83,7 @@ class TestSimpleSend(unittest.TestCase):
 
         extrinsic.encode(
             {
-                "account_id": "0x" + sending_pub,
+                "account_id": "0x" + seller_pub,
                 "signature_version": 1,
                 "signature": signature,
                 "call_function": call.value["call_function"],
@@ -108,32 +108,41 @@ class TestSimpleSend(unittest.TestCase):
 
 
 class TestMultiSigTrade(unittest.TestCase):
-    def test_approve_as_multi(self):
-        buyer_address = ss58_encode(buyer_keypair[0], 2)
-        sending_address = ss58_encode(seller_keypair[0], 2)
-        admin_address = ss58_encode(admin_keypair[0], 2)
-        sending_priv = seller_keypair[1].hex()
-        sending_pub = ss58_decode(sending_address)
+    def setUp(self):
+        self.buyer_address = ss58_encode(buyer_keypair[0], 2)
+        self.seller_address = ss58_encode(seller_keypair[0], 2)
+        self.admin_address = ss58_encode(admin_keypair[0], 2)
+        self.seller_priv = seller_keypair[1].hex()
+        self.seller_pub = ss58_decode(self.seller_address)
+        self.admin_priv = admin_keypair[1].hex()
+        self.admin_pub = ss58_decode(self.admin_address)
 
         substrate.init_runtime(block_hash=None)
 
-        inner_call = ScaleDecoder.get_decoder_class(
+        # Prepare the inner call as we will reuse it, lets not write code twice
+        self.inner_call = ScaleDecoder.get_decoder_class(
             "Call", metadata=substrate.metadata_decoder
         )
 
-        inner_call.encode(
+        self.inner_call.encode(
             {
                 "call_module": "Balances",
                 "call_function": "transfer",
-                "call_args": {"dest": buyer_address, "value": trade_value},
+                "call_args": {"dest": self.buyer_address, "value": trade_value},
             }
         )
 
+        # Figure out how to set these automatically
+        self.multi_tx_blocknum = 2290447
+        self.multi_tx_index = 2
+
+    def test_approve_as_multi(self):
+        # Function that hashes call with blake2_256
         def hash_call(call):
             call = bytes.fromhex(str(call.data)[2:])
             return f"0x{blake2_256(call)}"
 
-        hashed_call = hash_call(inner_call)
+        hashed_call = hash_call(self.inner_call)
         expected_hash = (
             "0xf526cf0bfe78fcc5c2d69a44c7b10ca9533cadb95ed94fdcb1da14b50f3a27b0"
         )
@@ -151,13 +160,15 @@ class TestMultiSigTrade(unittest.TestCase):
                 "call_args": {
                     "call_hash": hashed_call,
                     "maybe_timepoint": None,
-                    "other_signatories": [admin_address, buyer_address],
+                    "other_signatories": [self.admin_address, self.buyer_address],
                     "threshold": 2,
                 },
             }
         )
 
-        response = substrate.get_runtime_state("System", "Account", [sending_address])
+        response = substrate.get_runtime_state(
+            "System", "Account", [self.seller_address]
+        )
         assert response.get("result")
         nonce = response["result"].get("nonce", 0)
         genesis_hash = substrate.get_block_hash(0)
@@ -187,7 +198,7 @@ class TestMultiSigTrade(unittest.TestCase):
             data = data.encode()
 
         signature = sr25519.sign(
-            (bytes.fromhex(sending_pub), bytes.fromhex(sending_priv)), data
+            (bytes.fromhex(self.seller_pub), bytes.fromhex(self.seller_priv)), data
         )
 
         signature = "0x{}".format(signature.hex())
@@ -199,7 +210,94 @@ class TestMultiSigTrade(unittest.TestCase):
 
         extrinsic.encode(
             {
-                "account_id": "0x" + sending_pub,
+                "account_id": "0x" + self.seller_pub,
+                "signature_version": 1,
+                "signature": signature,
+                "call_function": outer_call.value["call_function"],
+                "call_module": outer_call.value["call_module"],
+                "call_args": outer_call.value["call_args"],
+                "nonce": nonce,
+                "era": "00",
+                "tip": 0,
+            }
+        )
+
+        """
+        # This is how we would broadcast
+        response = substrate.rpc_request(
+            "author_submitExtrinsic", [str(extrinsic.data)]
+        )
+        print(response)
+        extrinsic_hash = response["result"]
+        print("Extrinsic sent: {}".format(extrinsic_hash))
+        """
+
+    def test_approve_as_multi(self):
+        outer_call = ScaleDecoder.get_decoder_class(
+            "Call", metadata=substrate.metadata_decoder
+        )
+
+        outer_call.encode(
+            {
+                "call_module": "Utility",
+                "call_function": "as_multi",
+                "call_args": {
+                    "call": self.inner_call.serialize(),
+                    "maybe_timepoint": {
+                        "height": self.multi_tx_blocknum,
+                        "index": self.multi_tx_index,
+                    },
+                    "other_signatories": [self.buyer_address, self.seller_address],
+                    "threshold": 2,
+                },
+            }
+        )
+
+        response = substrate.get_runtime_state(
+            "System", "Account", [self.admin_address]
+        )
+        assert response.get("result")
+        nonce = response["result"].get("nonce", 0)
+        genesis_hash = substrate.get_block_hash(0)
+
+        era = "00"
+
+        # Create signature payload
+        signature_payload = ScaleDecoder.get_decoder_class("ExtrinsicPayloadValue")
+
+        signature_payload.encode(
+            {
+                "call": str(outer_call.data),
+                "era": era,
+                "nonce": nonce,
+                "tip": 0,
+                "specVersion": substrate.runtime_version,
+                "genesisHash": genesis_hash,
+                "blockHash": genesis_hash,
+            }
+        )
+
+        # Sign payload
+        data = str(signature_payload.data)
+        if data[0:2] == "0x":
+            data = bytes.fromhex(data[2:])
+        else:
+            data = data.encode()
+
+        signature = sr25519.sign(
+            (bytes.fromhex(self.admin_pub), bytes.fromhex(self.admin_priv)), data
+        )
+
+        signature = "0x{}".format(signature.hex())
+
+        # Create extrinsic
+        extrinsic = ScaleDecoder.get_decoder_class(
+            "Extrinsic", metadata=substrate.metadata_decoder
+        )
+
+        extrinsic.encode(
+            {
+                "account_id": "0x" + self.admin_pub,
                 "signature_version": 1,
                 "signature": signature,
                 "call_function": outer_call.value["call_function"],
