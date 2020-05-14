3c873b0fd79151b45cab570999197e19923dac8d
Ravi Ojha <ivarojha@gmail.com>
2020-05-14 11:35:22 +0530

    Add dotenv for python and move util to get seed (#19)
    * Add py dotenv package to easily manage keys
    
    * Add dotenv for python and move util to get seed

--- settings.py
+++ settings.py
@@ -1,7 +1,17 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+env_path = Path("js-examples/.env")
+load_dotenv(dotenv_path=env_path)
+
 NODE_URL = "wss://kusama-rpc.polkadot.io/"
 
-escrow_address = "HmGLJ6sG34vyBwyQSJWvN8HUByatcisuoyuyDXS4JeUZScm"
-seller_hexseed = "92406e11ce4cccd7fcb7fc5ead07b20535ad5ac65466a0ffce37d786b2c46c4e"
-buyer_hexseed = "41a2399f4cfc58f25a7bc2c0c65ced3335f2378245df71a9acd312564a3cdcff"
-admin_hexseed = "d3135d6f472edd024bb32b479eb68cb3d27532f42aad5b6825baa66911c619c1"
+
+escrow_address = os.getenv("ESCROW_ADDRESS")
+seller_hexseed = os.getenv("SELLER_HEX_SEED")
+buyer_hexseed = os.getenv("BUYER_HEX_SEED")
+admin_hexseed = os.getenv("ADMIN_HEX_SEED")
 trade_value = 10000000000
